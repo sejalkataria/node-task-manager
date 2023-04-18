97bab899f48dabea77b736b3ddf5aca7d527c516
sejalkataria <katariasejal18@gmail.com>
2023-04-18 12:16:46 +0530

    set up env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const connection = async () => {
-    const connect = await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
+    const connect = await mongoose.connect(process.env.MONGODB_URL)
     return connect
 }
 connection().then((result) => {
     console.log("connection SuccessFull")
 }).catch((e) => {
-        console.log(e)
-    })
+    console.log(e)
+})
--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
+//To create new user
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
@@ -17,6 +18,7 @@ router.post('/users', async (req, res) => {
     }
 })
 
+//To login a user
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
@@ -28,6 +30,7 @@ router.post('/users/login', async (req, res) => {
 
 })
 
+//To logout the user in existing device
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
@@ -41,21 +44,25 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+
+//To logout user from all devices
 router.post('/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.tokens = []
         await req.user.save()
         res.send()
     } catch (error) {
-        res.status(500).save()
+        res.status(500).send()
     }
 })
 
+//To get user's own profile
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 
 })
 
+//To Update user's profile
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ["name", "email", "age", "password"]
@@ -73,6 +80,7 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
+//To delete user's profile
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await User.deleteOne(req.user)
@@ -95,6 +103,7 @@ const upload = multer({
     }
 })
 
+//To upload user's profile picture
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
     const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
     req.user.avatar = buffer
@@ -105,12 +114,15 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
         res.status(400).send({ error: error.message })
     })
 
+
+//To delete user's profile picture
 router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
 })
 
+//To get user's profile picture
 router.get('/users/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
