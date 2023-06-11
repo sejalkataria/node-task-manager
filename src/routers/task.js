0e332a9f26a2f1234196cc380f102eab631981e4
sejalkataria <katariasejal18@gmail.com>
2023-06-11 11:34:39 +0530

    solved error while sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,7 +25,7 @@ router.get('/tasks', auth, async (req, res) => {
         match.completed = req.query.completed === 'true'
     }
     if (req.query.sortBy) {
-        const parts = req.query.split(':')
+        const parts = req.query.sortBy.split(':')
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try {
